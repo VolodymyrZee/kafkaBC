899e9858673b9abded4ef3da02e5a76a42fd6ea6
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-05-12 14:40:57 -0500

    consdemogroups

--- src/main/java/com/git/vz/kafka/tutorial1/ConsumerDemoGroups.java
+++ src/main/java/com/git/vz/kafka/tutorial1/ConsumerDemoGroups.java
@@ -15,13 +15,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
 
-public class ConsumerDemo {
+public class ConsumerDemoGroups {
 
     public static void main(String[] args) {
 
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
         String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "my-3rd-application";
+        String groupId = "my-6th-application";
         String topic = "first_topic";
 
 
@@ -45,8 +45,8 @@ public class ConsumerDemo {
         //poll for new data
 
         while (true) {
-          ConsumerRecords<String, String> records =
-                  consumer.poll(Duration.ofMillis(100)); //new in Kafka 2.0.0
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); //new in Kafka 2.0.0
             for (ConsumerRecord<String, String> record : records ) {
                 logger.info("Key: " + record.key() + ", Value: " + record.value());
                 logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
