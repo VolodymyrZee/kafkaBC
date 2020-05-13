43ccef20927d284d536041b606be2c25257fbbe9
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-05-13 15:54:25 -0500

    twitter client

--- src/main/java/com/git/vz/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/git/vz/kafka/tutorial2/TwitterProducer.java
@@ -10,12 +10,17 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
+
+    Logger logger= LoggerFactory.getLogger(TwitterProducer.class.getName());
 
     public TwitterProducer(){}
 
@@ -24,11 +29,33 @@ public class TwitterProducer {
     }
 
     public void run(){
-        // create a twitter account
+
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+
+        // create a twitter client
+  Client client = createTwitterClient(msgQueue);
+        // Attempts to establish a connection.
+        client.connect();
 
         //create a kafka producer
 
         //loop to send tweets to kafka
+
+        // on a different thread, or multiple different threads....
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null) {
+                logger.info(msg);
+            }
+
+        }
     }
 
     String consumerKey = "q0ZwbwMZviRexOHm5YgR4q4Bg";
@@ -37,9 +64,8 @@ public class TwitterProducer {
     String secret = "HRXuSpkfGsS3SWPPLKJBrWjLaBniDsHZCrxYKufdREn0U";
 
 
-    public void createTwitterClient (){
-        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+    public Client createTwitterClient (BlockingQueue<String> msgQueue){
+
 
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
@@ -60,8 +86,8 @@ public class TwitterProducer {
 
 
         Client hosebirdClient = builder.build();
-// Attempts to establish a connection.
-        hosebirdClient.connect();
+        return hosebirdClient;
+
 
     }
 
